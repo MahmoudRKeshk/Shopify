18788a8b79f977442aef419338f7183025784b15
Mahmoud R. Keshk <mahmoudredakeshk@gmail.com>
2024-05-05 03:50:36 +0300

    Adding the categories table to the database

--- Shopify/Data/Migrations/20240505004905_adding the categories table.Designer.cs
+++ Shopify/Data/Migrations/20240505004905_adding the categories table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shopify.Data;
 
@@ -11,9 +12,10 @@ using Shopify.Data;
 namespace Shopify.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240505004905_adding the categories table")]
+    partial class addingthecategoriestable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Shopify/Data/Migrations/20240505004905_adding the categories table.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shopify.Data.Migrations
+{
+    public partial class addingthecategoriestable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Category",
+                table: "Category");
+
+            migrationBuilder.RenameTable(
+                name: "Category",
+                newName: "categories");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_categories",
+                table: "categories",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_categories",
+                table: "categories");
+
+            migrationBuilder.RenameTable(
+                name: "categories",
+                newName: "Category");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Category",
+                table: "Category",
+                column: "Id");
+        }
+    }
+}
