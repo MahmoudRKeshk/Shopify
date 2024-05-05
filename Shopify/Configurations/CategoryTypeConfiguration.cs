1c54ec3c313027489dd3b3a047640ba781c9c94e
Mahmoud R. Keshk <mahmoudredakeshk@gmail.com>
2024-05-05 03:14:44 +0300

    Adding the category model with its cofigration file

---
+++ Shopify/Configurations/CategoryTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Shopify.Configrations
+{
+    public class CategoryTypeConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(p => p.Name).IsRequired();
+            builder.Property(p => p.Name).HasMaxLength(50);
+            builder.Property(p => p.CreatedOn).HasDefaultValueSql("GETDATE()");  
+        }
+    }
+}
